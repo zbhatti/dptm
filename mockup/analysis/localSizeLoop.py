65cf6c88d116ed41303a399a24e03ef9a91365de
zbhatti <djzubah@gmail.com>
2014-09-12 15:14:26 -0500

    fixed ROOT errors in localSizeLoop and added more data points

--- mockup/analysis/localSizeLoop.py
+++ mockup/analysis/localSizeLoop.py
@@ -2,49 +2,43 @@ from ROOT import *
 import numpy as np
 import os
 
-FileNames=[]
-#find files that end with .log and add them into FileNames array
-for root, dirs, files in os.walk("."):
-	for file in files:
-		if file.endswith(".log"):
-			print os.path.join(root, file)
-			FileNames.append(os.path.join(root, file))
-
 #dictionary of canvases with filenames as key
 Canvases={}
 
-#dictionary of graphs with filenames as key //Graphs[filename] = TGRAPH2D(
-Graphs={}
+#data[filename]=[tree, Bounds[threads[]], Hists[threads[]], MeanRMS[threads[]], canvas, TGRAPHS]
+data={}
 
-#dictionary of dictionaries of histograms with filenames and ordered pair as key //Hists[filename][(x,y,z,)] = 
-#Hists={}
-
-data={}#data[filename]=[tree, Bounds[threads[]], Hists[threads[]], MeanRMS[threads[]], canvas, TGRAPHS]
-
+def getBounds(tree):
+	Bounds= {}
+	threads=[]
+	for i in xrange(0,tree.GetEntries()):
+		tree.GetEntry(i)
+		if(tree.execute < 0): # the current test at xLocal is a failed test
+			continue
+	
+		wgDim = (tree.xLocal, tree.yLocal, tree.zLocal)
+		
+		if not (wgDim in threads):
+			threads.append(wgDim)
+			Bounds[wgDim]=[100000,0]
+		
+		if tree.execute>Bounds[wgDim][1]:
+			Bounds[wgDim][1]=tree.execute
+		if tree.execute<Bounds[wgDim][0]:
+			Bounds[wgDim][0]=tree.execute
+	return [Bounds, threads]
 
 def plot_ONE_D(f,tree, optimalFile,devName,kerName):
-	threads=[] #array of xLocal
-	Bounds={} #dictionary with tuples (xLocal) as keys and [min,max] as values
 	Hists={}
 	MeanRMS={}
 	min =1000000000
 	minP = 1
 	Canvases[f]=TCanvas(f)
 	
-	for i in xrange(0,tree.GetEntries()):
-		tree.GetEntry(i)
-		if(tree.execute < 0): # the current test at xLocal is a failed test
-			continue
-		X=tree.xLocal
-		
-		if not (X in threads):
-			threads.append(X)
-			Bounds[X]=[100000,0]
-		
-		if tree.execute>Bounds[X][1]:
-			Bounds[X][1]=tree.execute
-		if tree.execute<Bounds[X][0]:
-			Bounds[X][0]=tree.execute
+	#get maximum and minimum times found for (xLocal,1,1)
+	ret = getBounds(tree)
+	Bounds = ret[0] #dictionary with tuples (X,1,1) as keys and [min,max] as values
+	threads = ret[1] #array of tuples (xLocal,1,1)
 	
 	Canvases[f].cd()
 	x = np.zeros(len(threads), dtype=float) #xLocal
@@ -57,21 +51,21 @@ def plot_ONE_D(f,tree, optimalFile,devName,kerName):
 		if not thr in Hists:
 			Hists[thr]= -1
 		
-		histname="execute"+str(thr)
+		histname="execute,"+kerName+", "+str(tree.MB)+", "+devName+str(thr)
 		h=TH1F(histname, histname, len(threads), Bounds[thr][0]*.9, Bounds[thr][1]*1.1)
-		#CHECK IF preferred is there in tree.dir: and compare requested and actual within
-		tree.Draw("execute>>"+histname, "xLocal==" + str(thr))
+		tree.Draw("execute>>"+histname, "xLocal==" + str(thr[0]))
+		
 		MeanRMS[thr][0]=h.GetMean()
 		MeanRMS[thr][1]=h.GetRMS()
 		
 		Hists[thr]= h
 		
 		#print str(thr[0])+str(thr[1])+str(h.GetMean())
-		x[i]= thr
+		x[i]= thr[0]
 		t[i]= h.GetMean()
 		if h.GetMean() < min:
 			min = h.GetMean()
-			minP = thr
+			minP = thr[0]
 		i=i+1
 	
 	minString= "min at: " + str(minP)+" in: " + str("%.2f"%min) + " ms"
@@ -98,30 +92,16 @@ def plot_ONE_D(f,tree, optimalFile,devName,kerName):
 	
 	
 def plot_TWO_D(f,tree, optimalFile,devName,kerName):
-	threads=[] #array of tuples (xLocal,yLocal)
-	Bounds={} #dictionary with tuples (X,Y) as keys and [min,max] as values
 	Hists={}
 	MeanRMS={}
 	min =1000000000
 	minP = (0,0)
 	Canvases[f]=TCanvas(f)
 	
-	#get maximum and minimum times found for (xLocal,yLocal)
-	for i in xrange(0,tree.GetEntries()):
-		tree.GetEntry(i)
-		if(tree.execute < 0 ): #the current x,y point is a failed test
-			continue
-		X=tree.xLocal
-		Y=tree.yLocal
-		
-		if not ((X,Y) in threads):
-			threads.append((X,Y))
-			Bounds[(X,Y)]=[100000,0]
-		
-		if tree.execute>Bounds[(X,Y)][1]:
-			Bounds[(X,Y)][1]=tree.execute
-		if tree.execute<Bounds[(X,Y)][0]: 
-			Bounds[(X,Y)][0]=tree.execute
+	#get maximum and minimum times found for (xLocal,yLocal,1)
+	ret = getBounds(tree)
+	Bounds = ret[0] #dictionary with tuples (X,Y,1) as keys and [min,max] as values
+	threads = ret[1] #array of tuples (xLocal,yLocal,1)
 	
 	Canvases[f].cd()
 	x = np.zeros(len(threads), dtype=float)
@@ -136,9 +116,10 @@ def plot_TWO_D(f,tree, optimalFile,devName,kerName):
 		if not thr in Hists:
 			Hists[thr]= -1
 		
-		histname="execute"+str(thr[0])+str(thr[1])
+		histname="execute,"+kerName+", "+str(tree.MB)+", "+devName+str(thr)
 		h=TH1F(histname, histname, len(threads), Bounds[thr][0]*.9, Bounds[thr][1]*1.1)
 		tree.Draw("execute>>"+histname, "xLocal==" + str(thr[0]) + "&&" + "yLocal==" + str(thr[1]) )
+		
 		MeanRMS[thr][0]=h.GetMean()
 		MeanRMS[thr][1]=h.GetRMS()
 		
@@ -158,8 +139,9 @@ def plot_TWO_D(f,tree, optimalFile,devName,kerName):
 	
 	tree.GetEntry(0)
 	
+	graphName = kerName+", "+devName+", "+str(tree.MB)
 	optimalFile.write(kerName+","+devName+","+str(minP[0])+","+str(minP[1])+","+"1"+","+str(tree.MB)+","+str(tree.execute)+"\n")
-	plot = TGraph2D("empty","empty", len(threads), x, y, t)
+	plot = TGraph2D(graphName,"empty", len(threads), x, y, t)
 	title = ""+tree.kernel[:-1]+devName+str(tree.MB)
 	plot.SetTitle(title)
 	plot.SetMarkerStyle(20)
@@ -185,31 +167,16 @@ def plot_TWO_D(f,tree, optimalFile,devName,kerName):
 	
 
 def plot_THREE_D(f,tree, optimalFile,devName,kerName):
-	threads=[] #array of tuples (xLocal,yLocal,zLocal)
-	Bounds={} #dictionary with tuple keys. (X,Y,Z) is key and [min,max] is value
 	Hists={}
 	MeanRMS={}
 	min =1000000000
 	minP = (0,0,0)
 	Canvases[f]=TCanvas(f)
 	
-	#get maximum and minimum times found for (xLocal,yLocal,zLocal)
-	for i in xrange(0,tree.GetEntries()):
-		tree.GetEntry(i)
-		if(tree.execute < 0 ): #the current x,y,z point is a failed test
-			continue
-		X=tree.xLocal
-		Y=tree.yLocal
-		Z=tree.zLocal
-		
-		if not ((X,Y,Z) in threads):
-			threads.append((X,Y,Z))
-			Bounds[(X,Y,Z)]=[100000,0]
-		
-		if tree.execute>Bounds[(X,Y,Z)][1]:
-			Bounds[(X,Y,Z)][1]=tree.execute
-		if tree.execute<Bounds[(X,Y,Z)][0]: 
-			Bounds[(X,Y,Z)][0]=tree.execute
+	#get maximum and minimum times found for (xLocal,yLocal,zLocal), and get list of wgSizes
+	ret = getBounds(tree)
+	Bounds = ret[0] #dictionary with tuple keys. (X,Y,Z) is key and [min,max] is value
+	threads = ret[1] #array of tuples (xLocal,yLocal,zLocal)
 	
 	Canvases[f].cd()
 	xThreads = np.zeros(len(threads), dtype=float)
@@ -226,7 +193,7 @@ def plot_THREE_D(f,tree, optimalFile,devName,kerName):
 		if not thr in Hists:
 			Hists[thr]= -1
 		
-		histname="execute"+str(thr[0])+str(thr[1])+str(thr[2])
+		histname="execute,"+kerName+", "+str(tree.MB)+", "+devName+str(thr)
 		h=TH1F(histname, histname, len(threads), Bounds[thr][0]*.9, Bounds[thr][1]*1.1)
 		tree.Draw("execute>>"+histname, "xLocal==" + str(thr[0]) + "&&" + "yLocal==" + str(thr[1]) + "&&" + "zLocal==" + str(thr[2]))
 		MeanRMS[thr][0]=h.GetMean()
@@ -269,15 +236,22 @@ def plot_THREE_D(f,tree, optimalFile,devName,kerName):
 	
 	data[f]=(tree, Bounds, Hists, MeanRMS, Canvases[f], ntuple)
 
-#make ttree for each device/kernel.log
 
+FileNames=[]
+#find files that end with .log and add them into FileNames array
+for root, dirs, files in os.walk("."):
+	for file in files:
+		if file.endswith(".log"):
+			print os.path.join(root, file)
+			FileNames.append(os.path.join(root, file))	
+
+#make ttree for each device/kernel.log
 newFile=open("optimal.csv","w")
 newFile.write("kernel/C,device/C,x/I,y/I,z/I,mb/F,time/F\n")
 newFile.close() #used to clear the preexisting file
 
-
 for f in FileNames:
-	print "Working on " + f
+	print "Working on " + f + "..."
 	data[f]=()
 	tree=TTree()
 	tree.ReadFile(f,"",',')
